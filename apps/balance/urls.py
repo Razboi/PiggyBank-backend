ca591f15a269bbb5122b383f32e76b00084b92dd
Marc <recatala.marc@gmail.com>
2018-02-27 21:13:28 +0100

    Added url and view for deleting transactions and finished logic for updating

--- apps/balance/urls.py
+++ apps/balance/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (TransactionsCreateView, BalanceRetrieveView, AllTransactionsView,
-CreateTransaction, UpdateTransaction)
+CreateTransaction, UpdateTransaction, DeleteTransaction)
 from rest_framework.authtoken import views
 
 app_name = "balance"
@@ -10,5 +10,6 @@ urlpatterns = [
     path("all-transactions", AllTransactionsView.as_view(), name="all-transactions"),
     path("create-transaction", CreateTransaction.as_view(), name="create-transaction"),
     path("update-transaction", UpdateTransaction.as_view(), name="update-transaction"),
+    path("delete-transaction", DeleteTransaction.as_view(), name="delete-transaction"),
     path("total", BalanceRetrieveView.as_view(), name="balance-retrieve"),
 ]
--- apps/balance/views.py
+++ apps/balance/views.py
@@ -24,29 +24,52 @@ class CreateTransaction(APIView):
         new_transaction.save()
         return HttpResponse(new_transaction)
 
+class DeleteTransaction(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def delete(self, request):
+        transaction = Transaction.objects.get(pk=request.data["pk"])
+        balance = Balance.objects.get(pk=transaction.balance.pk)
+        # we must undo the transaction on the balance before deleting it
+        # if the amount was negative make it positive and add it
+        if transaction.amount < 0:
+            balance.totalBalance += -transaction.amount
+        # else substract it
+        else:
+            balance.totalBalance -= transaction.amount
+        balance.save()
+        transaction.delete()
+        return HttpResponse("Deleted")
+
 class UpdateTransaction(APIView):
     permission_classes = (permissions.IsAuthenticated,)
 
     def put(self, request):
         transaction = Transaction.objects.get(pk=request.data["pk"])
         balance = Balance.objects.get(pk=transaction.balance.pk)
-        # if the amount has changed undo the changes to the balance
+        # if the amount has changed recalculate the balance
+        # and recalculate the new currentTotal of the transaction
         if transaction.amount != request.data["amount"]:
             # if the old amount was negative make it positive and add it
             if transaction.amount < 0:
                 balance.totalBalance += -transaction.amount
-                balance.save()
+                new_trans_balance = transaction.currentTotal + -transaction.amount
             # else substract it
             else:
                 balance.totalBalance -= transaction.amount
-            # then add the new amount
+                new_trans_balance = transaction.currentTotal - transaction.amount
+            # now we have removed the old amount on the totalBalance and we can
+            # add the new amount
             balance.totalBalance += request.data["amount"]
             balance.save()
-
+        # now we have the currentTotal before it was added the amount and we can
+        # add the new amount
+        new_trans_balance += request.data["amount"]
         # Using update() instead of save() avoids triggering the post_save method
         Transaction.objects.filter(pk=request.data["pk"]).update(
             description=request.data["description"],
-            amount=request.data["amount"]
+            amount=request.data["amount"],
+            currentTotal=new_trans_balance
         )
 
         return HttpResponse(transaction)
